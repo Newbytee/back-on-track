286a5cd9ce86fb27cac62f1481271ce2b0329925
Kir Neboula <newbie13xd@outlook.com>
2018-07-23 13:08:50 +0200

    Popup now only appears on websites in the blacklist

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-var backOnTrackPopup = document.createElement("DIV");
-backOnTrackPopup.style.height = "100px";
-backOnTrackPopup.style.width = "200px";
-backOnTrackPopup.style.backgroundColor = "#FF00FF";
-backOnTrackPopup.style.zIndex = "100";
-backOnTrackPopup.style.position = "fixed";
-backOnTrackPopup.style.top = "50px";
-backOnTrackPopup.style.left = "45%";
-document.body.appendChild(backOnTrackPopup);
-console.log("Script was run successfully");
+browser.storage.sync.get("urls").then(loadList, gotError);
+
+function createPopup() {
+    var backOnTrackPopup = document.createElement("DIV");
+    var backOnTrackPopupText = document.createElement("P");
+    var backOnTrackPopupTextArea = document.createElement("TEXTAREA");
+    backOnTrackPopup.style.height = "100px";
+    backOnTrackPopup.style.width = "200px";
+    backOnTrackPopup.style.backgroundColor = "#FF00FF";
+    backOnTrackPopup.style.zIndex = "100";
+    backOnTrackPopup.style.position = "fixed";
+    backOnTrackPopup.style.top = "50px";
+    backOnTrackPopup.style.left = "45%";
+    backOnTrackPopup.appendChild(backOnTrackPopupText);
+    backOnTrackPopup.appendChild(backOnTrackPopupTextArea);
+    document.body.appendChild(backOnTrackPopup);
+    console.log("Script was run successfully");
+}
+
+function loadList(item) {
+    for (var i = 0; i < item.urls.length; i++) {
+        if (item.urls[i] === window.location.host) {
+            console.log("hi");
+            createPopup();
+        }
+    }
+}
+
+function gotError(error) {
+    console.log(error);
+}
