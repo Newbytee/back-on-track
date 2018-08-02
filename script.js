4831d18676cc41797ab613ef0ef2b0553e32a753
Kir Neboula <newbie13xd@outlook.com>
2018-08-02 16:27:50 +0200

    Button now closes popup if text field isn't empty, plus that I switched over to a class

--- script.js
+++ script.js
@@ -4,42 +4,53 @@ if (typeof browser === "undefined") {
 
 browser.storage.sync.get("urls").then(loadList, gotError);
 
-function createPopup() {
-    const backOnTrackPopup = document.createElement("DIV");
-    const backOnTrackPopupText = document.createElement("P");
-    const backOnTrackPopupTextArea = document.createElement("TEXTAREA");
-    const backOnTrackPopupButton = document.createElement("BUTTON");
-    backOnTrackPopup.style.height = "400px";
-    backOnTrackPopup.style.width = "400px";
-    backOnTrackPopup.style.backgroundColor = "#FFF9F8";
-    backOnTrackPopup.style.zIndex = "100";
-    backOnTrackPopup.style.position = "fixed";
-    backOnTrackPopup.style.top = "50px";
-    backOnTrackPopup.style.left = "50%";
-    backOnTrackPopup.style.marginLeft = "-200px";
-    backOnTrackPopup.style.boxShadow = "1px 1px 15px 5px #333333AA";
-    backOnTrackPopupText.innerHTML = "Why are you visiting this website?";
-    backOnTrackPopupText.style.textAlign = "center";
-    backOnTrackPopupTextArea.style.width = "96%";
-    backOnTrackPopupTextArea.style.height = "72%";
-    backOnTrackPopupTextArea.style.border = "none";
-    backOnTrackPopupButton.innerHTML = "Submit";
-    backOnTrackPopupButton.style.marginTop = "2.4%";
-    backOnTrackPopup.appendChild(backOnTrackPopupText);
-    backOnTrackPopup.appendChild(backOnTrackPopupTextArea);
-    backOnTrackPopup.appendChild(backOnTrackPopupButton);
-    document.body.appendChild(backOnTrackPopup);
-    console.log("Script was run successfully");
-}
-
 function loadList(item) {
     for (let i = 0; i < item.urls.length; i++) {
         if (item.urls[i] === window.location.host || "www." + item.urls[i] === window.location.host) {
-            createPopup();
+            new BackOnTrackPopup();
         }
     }
 }
 
 function gotError(error) {
     console.log(error);
+}
+
+class BackOnTrackPopup {
+    constructor() {
+        this.backOnTrackPopup = document.createElement("DIV");
+        this.backOnTrackPopupText = document.createElement("P");
+        this.backOnTrackPopupTextArea = document.createElement("TEXTAREA");
+        this.backOnTrackPopupButton = document.createElement("BUTTON");
+        this.backOnTrackPopup.style.height = "400px";
+        this.backOnTrackPopup.style.width = "400px";
+        this.backOnTrackPopup.style.backgroundColor = "#FFF9F8";
+        this.backOnTrackPopup.style.zIndex = "100";
+        this.backOnTrackPopup.style.position = "fixed";
+        this.backOnTrackPopup.style.top = "50px";
+        this.backOnTrackPopup.style.left = "50%";
+        this.backOnTrackPopup.style.marginLeft = "-200px";
+        this.backOnTrackPopup.style.boxShadow = "1px 1px 15px 5px #333333AA";
+        this.backOnTrackPopupText.innerHTML = "Why are you visiting this website?";
+        this.backOnTrackPopupText.style.textAlign = "center";
+        this.backOnTrackPopupText.style.marginTop = "3%";
+        this.backOnTrackPopupText.style.marginBottom = "3%";
+        this.backOnTrackPopupTextArea.style.width = "96%";
+        this.backOnTrackPopupTextArea.style.height = "72%";
+        this.backOnTrackPopupTextArea.style.border = "none";
+        this.backOnTrackPopupButton.innerHTML = "Submit";
+        this.backOnTrackPopupButton.style.marginTop = "2.4%";
+        this.backOnTrackPopupButton.style.width = "20%";
+        this.backOnTrackPopupButton.style.marginLeft = "40%";
+        this.backOnTrackPopupButton.addEventListener("click", this.closePopUp.bind(this));
+        this.backOnTrackPopup.appendChild(this.backOnTrackPopupText);
+        this.backOnTrackPopup.appendChild(this.backOnTrackPopupTextArea);
+        this.backOnTrackPopup.appendChild(this.backOnTrackPopupButton);
+        document.body.appendChild(this.backOnTrackPopup);
+    }
+    closePopUp() {
+        if (this.backOnTrackPopupTextArea.value.length > 0) {
+            this.backOnTrackPopup.parentElement.removeChild(this.backOnTrackPopup);
+        }
+    }
 }
